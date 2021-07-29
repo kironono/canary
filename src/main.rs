c9f0915cc3a2250601db282b36c8ff712327279b
Junichi Kakisako <kilo.sw@gmail.com>
2021-07-29 23:33:48 +0900

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    "Hello World!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(web::scope("/app").route("/index.html", web::get().to(index)))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
